a25631a05968c2375850d4bfd7db9e09cae06795
ab-cio <ab-cio@users.noreply.github.com>
2016-10-31 02:48:09 -0400

    Implement test for __getitem__.

--- anagram/test.py
+++ anagram/test.py
@@ -2,6 +2,8 @@
 
 # pylint: disable=missing-docstring
 
+import io
+import random
 import unittest
 
 import anagram
@@ -9,8 +11,27 @@ import anagram
 
 class TestAnagramFinder(unittest.TestCase):
 
+    def setUp(self):
+        words = ['test',
+                 'elvis', 'lives',
+                 'evil', 'veil', 'vile',
+                 'spot',
+                 'coed', 'deco']
+        random.shuffle(words)
+        text = '\n'.join(words + [''])
+        file = io.StringIO(text)
+        self._anagram_finder = anagram.AnagramFinder(file)
+
     def test_getitem(self):
-        return anagram.AnagramFinder
+        anagrams = {'test': [],  # Present word with 0 anagrams
+                    'elvis': ['lives'],  # Present word with 1 anagram
+                    'evil': ['veil', 'vile'],  # Present word with >1 anagrams
+                    'fun': [],  # Missing word with 0 anagrams
+                    'post': ['spot'],  # Missing word with 1 anagram
+                    'code': ['coed', 'deco']}  # Missing word with >1 anagrams
+        for word, expected_anagrams in anagrams.items():
+            actual_anagrams = self._anagram_finder[word]
+            self.assertEqual(set(expected_anagrams), set(actual_anagrams))
 
 if __name__ == '__main__':
     unittest.main()
